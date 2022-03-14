634e735a22946b4fd379ed5c1b1fdc329c99d78b
Shoham Katzav <shohamkatzav95@gmail.com>
2022-03-14 20:23:34 +0200

    Add project files.

---
+++ ImplementingICollection/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using ImplementingICollection;
+class Program
+{
+    public static void Main()
+    {
+        string userInput;
+        bool endProgram = false;
+        UniqueCollection<string> MyUniqueCollection = new UniqueCollection<string>();
+        MyUniqueCollection.ExistItemAdded += Program.ExistItemAdded;
+        MyUniqueCollection.NotExistItemAdded += Program.NotExistItemAdded;
+        MyUniqueCollection.ExistItemRemoved += Program.ExistItemRemoved;
+        MyUniqueCollection.NotExistItemRemoved += Program.NotExistItemRemoved;
+        MyUniqueCollection.Add("Hello");
+        MyUniqueCollection.Add("World");
+        MyUniqueCollection.Add("Unique");
+        MyUniqueCollection.Add("Collection");
+        Console.Clear();
+        foreach (var item in MyUniqueCollection)
+            Console.WriteLine(item);
+
+        while (!endProgram)
+        {
+            Console.WriteLine(@"
+            Choose an action to perform:
+            A - Add
+            R - Remove
+            E - Enumerate
+            C - Contains
+            D - Clear
+            Q - Quit");
+            userInput = Console.ReadLine().ToLower().Trim();
+            switch(userInput)
+            {
+                case "a":
+                    Console.WriteLine("Enter item to add:");
+                    MyUniqueCollection.Add(Console.ReadLine());
+                    break;
+                case "r":
+                    Console.WriteLine("Enter item to remove:");
+                    MyUniqueCollection.Remove(Console.ReadLine());
+                    break;
+                case "e":
+                    Console.WriteLine("Enumerating:");
+                    foreach (var item in MyUniqueCollection)
+                        Console.WriteLine(item);
+                    break;
+                case "c":
+                    Console.WriteLine("Enter item to search:");
+                    Console.WriteLine($"Contains: {MyUniqueCollection.Contains(Console.ReadLine())}");
+                    break;
+                case "d":
+                    MyUniqueCollection.Clear();
+                    break;
+                case "q":
+                    endProgram = true;
+                    break;
+            }
+        }
+    }
+
+    public static void ExistItemAdded(object sender)
+    {
+        Console.WriteLine($"Cannot add [{sender}], the item already exist.");
+    }
+    public static void NotExistItemAdded(object sender)
+    {
+        Console.WriteLine($"[{sender}] added successfully.");
+    }
+
+    public static void ExistItemRemoved(object sender)
+    {
+        Console.WriteLine($"[{sender}] Removed successfully.");
+    }
+    public static void NotExistItemRemoved(object sender)
+    {
+        Console.WriteLine($"Cannot Remove [{sender}], the item isn't exist.");
+    }
+}
---
+++ ImplementingICollection/UniqueCollection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementingICollection
+{
+    internal class UniqueCollection<T>: ICollection<T>
+    {
+        public List<T> items = new List<T>();
+        public int Count => items.Count;
+        public event Action<T>? ExistItemAdded;
+        public event Action<T>? NotExistItemAdded;
+        public event Action<T>? ExistItemRemoved;
+        public event Action<T>? NotExistItemRemoved;
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+        public void Add(T itemToAdd)
+        {
+            if (!items.Contains(itemToAdd))
+            {
+                items.Add(itemToAdd);
+                if(NotExistItemAdded != null)
+                    NotExistItemAdded(itemToAdd);
+            }
+            else
+                if (ExistItemAdded != null)
+                    ExistItemAdded(itemToAdd);
+        }
+        public void Clear()
+        {
+            while (Count > 0)
+                items.RemoveAt(0);
+            Console.WriteLine("List Cleared");
+        }
+
+        public bool Contains(T item)
+        {
+            return items.Contains(item);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in items)
+                yield return item;
+        }
+        public bool Remove(T itemToRemove)
+        {
+            if (!items.Contains(itemToRemove))
+            {
+                if (NotExistItemRemoved != null)
+                    NotExistItemRemoved(itemToRemove);
+            }
+            else
+                ExistItemRemoved(itemToRemove);
+            return items.Remove(itemToRemove);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            array = items.GetRange(arrayIndex, items.Count).ToArray();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
